1105cce7df6fcc89182ce1e8e146f42583f13aa6
Thigo Dev <thiagomvk08@gmail.com>
2023-10-11 17:01:31 -0300

    commit inicial

---
+++ trabalho-api-01/src/main/java/br/com/api/g1/TrabalhoApi01Application.java
@@ -0,0 +1,13 @@
+package br.com.api.g1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoApi01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoApi01Application.class, args);
+	}
+
+}
---
+++ trabalho-api-01/src/test/java/br/com/api/g1/TrabalhoApi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.g1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoApi01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
