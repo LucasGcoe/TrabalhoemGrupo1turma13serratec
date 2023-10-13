e97ffdfa92695b78784b2729b61773a4f9790278
Thigo Dev <thiagomvk08@gmail.com>
2023-10-13 18:50:41 -0300

    build: criação e configuração do swagger

---
+++ trabalho-api-01/src/main/java/br/com/api/g1/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package br.com.api.g1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+		
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+	
+	public ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfoBuilder()
+				.title("Documentação da Api do Grupo 01")
+				.description("Projeto final Api 2023.2 Serratec")
+				.license("Apache license version 2.0")
+				.version("1.0.0")
+				.build();
+		return apiInfo;
+	}
+
+}
