d88edd2a27e8563c5f16bc431d568f7e99a52121
VitorMelloEsp <vitormello1602@gmail.com>
2023-10-13 17:43:08 -0300

    feat: implementando anotações e conexões na classe categoria.

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/CategoriaController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/CategoriaController.java
@@ -1,5 +1,18 @@
 package br.com.api.g1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.g1.services.CategoriaService;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+
+	
+	@Autowired
+	CategoriaService categoriaService;
 
 }
--- trabalho-api-01/src/main/java/br/com/api/g1/services/CategoriaService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/CategoriaService.java
@@ -1,5 +1,13 @@
 package br.com.api.g1.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.g1.repositories.CategoriaRepository;
+
+@Service
 public class CategoriaService {
 
+	@Autowired
+	CategoriaRepository categoriaRepository;
 }
