159fc9f085281a5d18caf0b5b94015923b8863da
VitorMelloEsp <vitormello1602@gmail.com>
2023-10-13 20:16:22 -0300

    feat:Criação dos Métodos GET e POST na classe Categoria.

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/CategoriaController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/CategoriaController.java
@@ -1,9 +1,15 @@
 package br.com.api.g1.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.g1.entities.Categoria;
 import br.com.api.g1.services.CategoriaService;
 
 @RestController
@@ -14,5 +20,15 @@ public class CategoriaController {
 	
 	@Autowired
 	CategoriaService categoriaService;
+	
+	@PostMapping("/salvarCategoria")
+	public Categoria salvarCategoria(@RequestBody Categoria categoria) {
+		return categoriaService.salvarCategoria(categoria);
+	}
+	
+	@GetMapping("/listarCategorias")
+	public List<Categoria> listarCategorias() {
+		return categoriaService.listarCategorias();
+	}
 
 }
--- trabalho-api-01/src/main/java/br/com/api/g1/services/CategoriaService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/CategoriaService.java
@@ -1,8 +1,11 @@
 package br.com.api.g1.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.api.g1.entities.Categoria;
 import br.com.api.g1.repositories.CategoriaRepository;
 
 @Service
@@ -10,4 +13,12 @@ public class CategoriaService {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	public Categoria salvarCategoria(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+	
+	public List<Categoria> listarCategorias() {
+		return categoriaRepository.findAll();
+	}
 }
