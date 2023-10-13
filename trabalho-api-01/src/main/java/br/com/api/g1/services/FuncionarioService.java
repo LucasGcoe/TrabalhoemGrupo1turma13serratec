a74b7392e293fcd2aaf825af35ed8212deec3fcc
Lucas-Jose-Goncalves <142056530+Lucas-Jose-Goncalves@users.noreply.github.com>
2023-10-13 17:12:00 -0300

    feat: implementando conexões e anotações na classe funcionario.

--- trabalho-api-01/src/main/java/br/com/api/g1/services/FuncionarioService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/FuncionarioService.java
@@ -1,5 +1,15 @@
 package br.com.api.g1.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.g1.repositories.FuncionarioRepository;
+
+@Service
 public class FuncionarioService {
+	
+	@Autowired
+	FuncionarioRepository funcionarioRepository; 
+
 
 }
