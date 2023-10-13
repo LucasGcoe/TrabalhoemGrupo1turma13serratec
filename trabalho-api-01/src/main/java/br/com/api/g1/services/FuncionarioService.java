381c2d833621919c2f4e1342679b0bfbd010d1af
Lucas-Jose-Goncalves <142056530+Lucas-Jose-Goncalves@users.noreply.github.com>
2023-10-13 20:08:56 -0300

    feat:Craição do Metodo Get da classe Funcionario

--- trabalho-api-01/src/main/java/br/com/api/g1/services/FuncionarioService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/FuncionarioService.java
@@ -1,5 +1,7 @@
 package br.com.api.g1.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,10 @@ public class FuncionarioService {
 
 	public Funcionario salvarFuncionario(Funcionario funcionario) {
 		return funcionarioRepository.save(funcionario);
+		}
+	
+	public List<Funcionario> listarFuncionarios(){
+			return funcionarioRepository.findAll();
+	
 	}
 }
