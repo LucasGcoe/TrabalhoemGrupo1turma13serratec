f313c1dbb9f39de6d573f4e4df0e832ed54203e2
Thigo Dev <thiagomvk08@gmail.com>
2023-10-13 21:06:24 -0300

    feat: criação dos métodos get e post na classe clientes

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/ClienteController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/ClienteController.java
@@ -1,9 +1,15 @@
 package br.com.api.g1.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.g1.entities.Cliente;
 import br.com.api.g1.services.ClienteService;
 
 @RestController
@@ -12,5 +18,15 @@ public class ClienteController {
 	
 	@Autowired
 	ClienteService clienteService;
+	
+	@PostMapping("/salvarCliente")
+	public Cliente salvarCliente(@RequestBody Cliente cliente) {
+		return clienteService.salvarCliente(cliente);
+	}
+	
+	@GetMapping("/listarClientes")
+	public List<Cliente> listarClientes(){
+		return clienteService.listarClientes();
+	}
 
 }
--- trabalho-api-01/src/main/java/br/com/api/g1/services/ClienteService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/ClienteService.java
@@ -1,8 +1,11 @@
 package br.com.api.g1.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.api.g1.entities.Cliente;
 import br.com.api.g1.repositories.ClienteRepository;
 
 @Service
@@ -10,4 +13,12 @@ public class ClienteService {
 
 	@Autowired
 	ClienteRepository clienteRepository;
+	
+	public Cliente salvarCliente(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	public List<Cliente> listarClientes(){
+		return clienteRepository.findAll();
+	}
 }
