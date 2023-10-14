5c23e9cc75182427020ba5f7d999a2cce1d47e0e
Lucas-Jose-Goncalves <142056530+Lucas-Jose-Goncalves@users.noreply.github.com>
2023-10-14 10:16:34 -0300

    feat: Criação dos metodos Put e Delete Logico na classe funcionario

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/FuncionarioController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/FuncionarioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,17 @@ public class FuncionarioController {
 	@DeleteMapping("/deletarIdFuncionarios/{id}")
 	public void deletarIdFuncionario(@PathVariable Integer id) {
 		funcionarioService.deletarIdFuncionario(id);
+	}
+	
+	@DeleteMapping("/desativarFuncionario/{id}")
+	public void desativarFuncionario(@PathVariable Integer id) {
+		funcionarioService.desativarFuncionario(id);
+	}
+	
+	
+	@PutMapping("/atualizarFuncionario/{id}")
+	public Funcionario atualizarFuncionario(@PathVariable Integer id, @RequestBody Funcionario funcionario) {
+		return funcionarioService.atualizarFuncionario(id,funcionario);
 	}
 	
 }
--- trabalho-api-01/src/main/java/br/com/api/g1/entities/Funcionario.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/entities/Funcionario.java
@@ -15,56 +15,61 @@ public class Funcionario {
 	private Integer id_funcionario;
 	private String	nome;
 	private String	cpf;
-	
-	
+	private Boolean ativo;
 	
 	public Funcionario() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-
-	public Funcionario(Integer id_funcionario, String nome, String cpf) {
+	public Funcionario(Integer id_funcionario, String nome, String cpf, Boolean ativo) {
 		super();
 		this.id_funcionario = id_funcionario;
 		this.nome = nome;
 		this.cpf = cpf;
+		this.ativo = ativo;
 	}
-
 
 	public Integer getId_funcionario() {
 		return id_funcionario;
 	}
 
-
 	public void setId_funcionario(Integer id_funcionario) {
 		this.id_funcionario = id_funcionario;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public String getCpf() {
 		return cpf;
 	}
 
-
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
 
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
 
 	@Override
 	public String toString() {
-		return "Funcionario [id_funcionario=" + id_funcionario + ", nome=" + nome + ", cpf=" + cpf + "]";
+		return "Funcionario [id_funcionario=" + id_funcionario + ", nome=" + nome + ", cpf=" + cpf + ", ativo=" + ativo
+				+ "]";
 	}
+
+	
+	
+	
 	
 }
