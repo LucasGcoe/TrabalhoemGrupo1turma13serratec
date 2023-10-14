b2e9121f01766cb1ae05527bbad542efe26c9300
Lucas <lucasoliveiragcoe@gmail.com>
2023-10-13 22:08:25 -0300

    feat: Criação de Get por id e Delete por id na classe produtos

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/ProdutoController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/ProdutoController.java
@@ -3,7 +3,9 @@ package br.com.api.g1.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,4 +30,13 @@ public class ProdutoController {
 		return produtoService.listarProdutos();
 	}
 
+	@GetMapping("/listarIdProdutos/{id}")
+	public Produto listarIdProdutos(@PathVariable Integer id) {
+		return produtoService.listarIdProdutos(id);
+	}
+	
+	@DeleteMapping("/deletarIdProdutos/{id}")
+	public void deletarIdProdutos(@PathVariable Integer id) {
+		produtoService.deletarIdProdutos(id);
+	}
 }
--- trabalho-api-01/src/main/java/br/com/api/g1/services/ProdutoService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/ProdutoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.api.g1.entities.Produto;
 import br.com.api.g1.repositories.ProdutoRepository;
@@ -22,5 +23,11 @@ public class ProdutoService {
 		return produtoRepository.findAll();
 	}
 
+	public Produto listarIdProdutos(Integer id) {
+		return produtoRepository.findById(id).get();
+	}
 	
+	public void deletarIdProdutos(Integer id) {
+		produtoRepository.deleteById(id);
+	}
 }
