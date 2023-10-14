6df4c47cd35113936a4c502e6e50ea9ac375a42e
Lucas-Jose-Goncalves <142056530+Lucas-Jose-Goncalves@users.noreply.github.com>
2023-10-13 22:16:22 -0300

    feat: Criação de Get por Id e Delete por Id na classe Funcionario

--- trabalho-api-01/src/main/java/br/com/api/g1/services/FuncionarioService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/FuncionarioService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.api.g1.entities.Funcionario;
 import br.com.api.g1.repositories.FuncionarioRepository;
@@ -22,4 +23,13 @@ public class FuncionarioService {
 			return funcionarioRepository.findAll();
 	
 	}
+	
+	public Funcionario listarIdFuncionario(Integer id) {
+		return funcionarioRepository.findById(id).get();
+	}
+	public void deletarIdFuncionario(@PathVariable Integer id) {
+		funcionarioRepository.deleteById(id);
+	}
+		
+		
 }
