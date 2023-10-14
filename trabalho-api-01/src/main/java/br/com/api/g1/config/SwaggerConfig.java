866c7ad9217dcdfbab564f7c2cbf5c90b968f329
VitorMelloEsp <vitormello1602@gmail.com>
2023-10-14 11:18:07 -0300

    feat: Atualização da versão.

--- trabalho-api-01/src/main/java/br/com/api/g1/config/SwaggerConfig.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
 				.title("Documentação da Api do Grupo 01")
 				.description("Projeto final Api 2023.2 Serratec")
 				.license("Apache license version 2.0")
-				.version("4.17.0")
+				.version("5.0.0")
 				.build();
 		return apiInfo;
 	}
