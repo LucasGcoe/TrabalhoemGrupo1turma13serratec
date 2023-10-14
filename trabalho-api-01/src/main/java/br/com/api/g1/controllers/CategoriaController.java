0dce5d56feda80707757848e39826d2033b5f15e
VitorMelloEsp <vitormello1602@gmail.com>
2023-10-14 10:07:25 -0300

    feat: Criação dos métodos PUT e DELETE lógico na classe categoria

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/CategoriaController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/CategoriaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,4 +44,13 @@ public class CategoriaController {
 		categoriaService.deletarIdCategoria(id);
 	}
 	
+	@DeleteMapping("/desativarCategoria/{id}")
+	public void desativarCategoria(@PathVariable Integer id) {
+		categoriaService.desativarCategoria(id);
+		}
+	
+	@PutMapping("/atualizarCategoria/{id}")
+	public Categoria atualizarCategoria(@PathVariable Integer id, @RequestBody Categoria categoria) {
+		return categoriaService.atualizarCategoria(id, categoria);
+	}
 }
