5960bc4fa488d26009cb8deea414ca562dcee458
Lucas-Jose-Goncalves <142056530+Lucas-Jose-Goncalves@users.noreply.github.com>
2023-10-18 22:24:23 -0300

    feat : criado metodo de envio teste.

--- trabalho-api-01/src/main/java/br/com/api/g1/services/EmailService.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/services/EmailService.java
@@ -1,4 +1,9 @@
 package br.com.api.g1.services;
+
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.MessagingException;
@@ -13,10 +18,14 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import br.com.api.g1.entities.User;
+
 @Configuration
 @Service
 public class EmailService {
 
+	@Autowired
+	UserService userService ;
 	private JavaMailSender emailSender;
 
     @Autowired
@@ -51,7 +60,7 @@ public class EmailService {
 		return emailSender;
 	}
 	
-	public void envioEmailCadastro() {
+	public void envioEmailCadastro() { // é esse método que vamos copiar 
 		MimeMessage mensagemCadastro = emailSender.createMimeMessage();
 
 		try {
@@ -80,6 +89,69 @@ public class EmailService {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public void envioEmailTeste() {
+		MimeMessage mensagemCadastro = emailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(mensagemCadastro, true);
+			helper.setFrom("apiserrag1@gmail.com");
+			helper.setTo("lucasjosegoncalves.original@gmail.com");
+			helper.setSubject("Cadastro concluido!");
+			
+			LocalDate localDate=LocalDate.now();
+			DateTimeFormatter format=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			String dataEntrega=localDate.plusDays(7).format(format);//simula uma data para entrega.
+			
+			Double valor=15.9;//Valor fixo so para exemplo. Se quiser usar devemos passar um "Pedido pedido" entre os parenteses do "public void envioEmailTeste"nesse caso. 
+			DecimalFormat df= new DecimalFormat("R$ ,##0.00");//Formata o valor do produto
+			
+			StringBuilder builder= new StringBuilder();
+			builder.append("<html>\r\n");
+			builder.append("<body>\r\n");
+			builder.append("<div align=\"center\">\r\n");
+			builder.append("<h1>Convite</h1>\r\n");
+			builder.append("</div>\r\n");
+			builder.append("<br/>\r\n");
+			
+			builder.append("<center>");
+			builder.append("<table border='2' cellpadding='4'>\r\n");
+			builder.append("<tr><th> Nome</th> <th> Email</th><th> Perfis</th><th> Data de Entrega</th> </tr> \r\n");
+			
+			List<User>listaUsuarios=userService.listarTodos();
+			for(User user : listaUsuarios) {
+				builder.append("		    <tr>\r\n");
+				builder.append("			<td>\r\n");
+				builder.append(user.getNomeUsuario());
+				builder.append("			</td>\r\n");
+				builder.append("			<td>\r\n");
+				builder.append(user.getEmail());
+				builder.append("			</td>\r\n");
+				builder.append("		    <td>\r\n");
+				builder.append(user.getRoles());
+				builder.append("			</td>\r\n");
+				builder.append("			<td>\r\n");
+				builder.append(dataEntrega);
+				builder.append("			</td>\r\n");
+			}
+			
+			builder.append("		</table>\r\n");
+			builder.append("		</center>\r\n");
+			builder.append("		<table border='1' cellpadding='1'  >\r\n");
+			builder.append("<tr><th>Valor Total</th></tr>\r\n");
+			builder.append("			<td>\r\n");
+			builder.append(df.format(valor));
+			builder.append("			</td>\r\n");
+			builder.append("		</table>\r\n");
+			builder.append("	</body>\r\n");
+			builder.append("</html>");
+			
+			helper.setText(builder.toString(), true);
+			emailSender.send(mensagemCadastro);
+			
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
