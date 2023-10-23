83d4c3463e175e87577d079323efe34fb8aaf7bc
Thigo Dev <thiagomvk08@gmail.com>
2023-10-23 20:22:12 -0300

    built: criação das classes de exceções em tratamento de erros

--- trabalho-api-01/src/main/java/br/com/api/g1/controllers/ProdutoController.java
+++ trabalho-api-01/src/main/java/br/com/api/g1/controllers/ProdutoController.java
@@ -3,6 +3,7 @@ package br.com.api.g1.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.g1.dto.MessageResponseDTO;
 import br.com.api.g1.entities.Produto;
 import br.com.api.g1.services.ProdutoService;
 
@@ -23,11 +25,13 @@ public class ProdutoController {
 	ProdutoService produtoService;
 
 	@PostMapping("/salvarProduto")
-	public Produto salvarProduto(@RequestBody Produto produto) {
-		return produtoService.salvarProduto(produto);
+	public ResponseEntity<MessageResponseDTO> salvarProduto(@RequestBody Produto produto) {
+		produtoService.salvarProduto(produto);
+		return ResponseEntity.ok(new MessageResponseDTO("Produto cadastrado com sucesso!"));
 	}
+
 	@GetMapping("/listarProdutos")
-	public List<Produto> listarProdutos(){
+	public List<Produto> listarProdutos() {
 		return produtoService.listarProdutos();
 	}
 
@@ -35,18 +39,19 @@ public class ProdutoController {
 	public Produto listarIdProdutos(@PathVariable Integer id) {
 		return produtoService.listarIdProdutos(id);
 	}
-	
+
 	@DeleteMapping("/deletarIdProdutos/{id}")
 	public void deletarIdProdutos(@PathVariable Integer id) {
 		produtoService.deletarIdProdutos(id);
 	}
+
 	@DeleteMapping("/desativarProduto/{id}")
 	public void desativarProduto(@PathVariable Integer id) {
 		produtoService.desativarProduto(id);
 	}
-	
+
 	@PutMapping("/atualizarProduto/{id}")
 	public Produto atualizarProduto(@PathVariable Integer id, @RequestBody Produto produto) {
 		return produtoService.atualizarProduto(id, produto);
-}
+	}
 }
